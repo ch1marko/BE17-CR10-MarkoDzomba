e27e31c17085e9a41a50e5126ffad5ee8e111ee3
Marko <marko.dzomba.info@gmail.com>
2022-11-11 19:53:01 +0100

    create

--- actions/a_create.php
+++ actions/a_create.php
@@ -1,53 +1,64 @@
 <?php
 require_once 'db_connect.php';
 require_once 'file_upload.php';
-
-if ($_POST) {  
-   $name = $_POST['name'];
-   $price = $_POST['price'];
-   $uploadError = '';
-   //this function exists in the service file upload.
-   $picture = file_upload($_FILES['picture']); 
-   
-   $sql = "INSERT INTO products (name, price, picture) VALUES ('$name', $price,'$picture->fileName')";
-
-   if (mysqli_query($connect, $sql) === true) {
-       $class = "success";
-       $message = "The entry below was successfully created <br>
+if ($_POST) {
+    $title = $_POST['title'];
+    $isbncode = $_POST['isbncode'];
+    $dis = $_POST['short_description'];
+    $type = $_POST['type'];
+    $author_first_name = $_POST['author_first_name'];
+    $author_last_name = $_POST['author_last_name'];
+    $publisher_name = $_POST['publisher_name'];
+    $publisher_address = $_POST['publisher_address'];
+    $publish_date = $_POST['publish_date'];
+    $status = $_POST['status'];
+    $uploadError = '';
+    $picture = file_upload($_FILES['pic']);
+    $sql = "INSERT INTO library(`title`, `isbncode`, `pic`, `short_description`, `type`, `author_first_name`, `author_last_name`, `publisher_name`, `publisher_address`, `publish_date`, `status`) VALUES ('$title', '$isbncode','$picture->fileName','$shortdescription', '$type', '$author_first_name', '$author_last_name', '$publisher_name', '$publisher_address', '$publish_date', '$status')";
+    if (mysqli_query($connect, $sql) === true) {
+        $class = "success";
+        $message = "The entry below was successfully created <br>
             <table class='table w-50'><tr>
-            <td> $name </td>
-            <td> $price </td>
+            <td> $title </td>
+            <td> $isbncode </td>
+            <td> $shortdescription </td>
+            <td> $type </td>
+            <td> $author_first_name </td>
+            <td> $author_last_name </td>
+            <td> $publisher_name </td>
+            <td> $publisher_address </td>
+            <td> $publish_date </td>
+            <td> $status </td>
             </tr></table><hr>";
-       $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
-   } else {
-       $class = "danger";
-       $message = "Error while creating record. Try again: <br>" . $connect->error;
-       $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
-   }
-   mysqli_close($connect);
+        $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
+        header("refresh:3;url= ../index.php");
+    } else {
+        $class = "danger";
+        $message = "Error while creating record. Try again: <br>" . $connect->error;
+        $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
+    }
+    mysqli_close($connect);
 } else {
-   header("location: ../error.php");
+    header("location: ../error.php");
 }
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-       <meta charset="UTF-8">
-       <title>Update</title>
-       <?php require_once '../components/boot.php'?>
-   </head>
-   <body>
-       <div class="container">
-           <div class="mt-3 mb-3">
-               <h1>Create request response</h1>
-           </div>
-           <div class="alert alert-<?=$class;?>" role="alert">
-               <p><?php echo ($message) ?? ''; ?></p>
-               <p><?php echo ($uploadError) ?? ''; ?></p>
-               <a href='../index.php'><button class="btn btn-primary" type='button'>Home</button></a>
-           </div>
-       </div>
-   </body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Update</title>
+        <?php require_once '../components/boot.php'?>
+    </head>
+    <body>
+        <div class="container">
+            <div class="mt-3 mb-3">
+                <h1>Create request response</h1>
+            </div>
+            <div class="alert alert-<?=$class;?>" role="alert">
+                <p><?php echo ($message) ?? ''; ?></p>
+                <p><?php echo ($uploadError) ?? ''; ?></p>
+                <a href='../index.php'><button class="btn btn-primary" type='button'>Home</button></a>
+            </div>
+        </div>
+    </body>
 </html>
